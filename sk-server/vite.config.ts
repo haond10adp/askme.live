c30c2ca0411b6c3229fef95f4bf66dc34aaf8a3d
Hao <haond10adp@gmail.com>
2024-11-25 17:15:27 +0700

    23

--- sk-server/vite.config.ts
+++ sk-server/vite.config.ts
@@ -1,13 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-// import fs from 'fs';
+import fs from 'fs';
 
 export default defineConfig({
-	// server: {
-	// 	https: {
-	// 		key: fs.readFileSync('../.cert/private.key'),
-	// 		cert: fs.readFileSync('../.cert/certificate.crt')
-	// 	}
-	// },
+	server: {
+		https: {
+			key: fs.readFileSync('../.cert/private.key'),
+			cert: fs.readFileSync('../.cert/certificate.crt')
+		}
+	},
 	plugins: [sveltekit()]
 });
