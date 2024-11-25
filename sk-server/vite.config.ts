63089f6577045bf7ccf6fd3c8d82644e888ca69a
Hao <haond10adp@gmail.com>
2024-11-25 13:52:05 +0700

    11

--- sk-server/vite.config.ts
+++ sk-server/vite.config.ts
@@ -1,7 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import fs from 'fs';
 
 export default defineConfig({
-	plugins: [sveltekit(), basicSsl()]
+	server: {
+		https: {
+			key: fs.readFileSync('../.cert/key.pem'),
+			cert: fs.readFileSync('../.cert/cert.pem')
+		}
+	},
+	plugins: [sveltekit()]
 });
