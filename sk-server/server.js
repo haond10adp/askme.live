bde0b3be112fd082db025532d3e57e5dfb20be76
Hao <haond10adp@gmail.com>
2024-12-09 18:16:49 +0700

    43

--- sk-server/server.js
+++ sk-server/server.js
@@ -1,7 +1,6 @@
 import { handler } from './build/handler.js';
 import express from 'express';
 import fs from 'fs';
-import http from 'http';
 import https from 'https';
 
 const privateKey = fs.readFileSync('../.cert/private.pem', 'utf8');
@@ -10,15 +9,10 @@ const credentials = { key: privateKey, cert: certificate };
 
 const app = express();
 
-const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
 
 const PORT = 80;
 const SSLPORT = 443;
-
-httpServer.listen(PORT, function () {
-	console.log('HTTP Server is running on: http://localhost:%s', PORT);
-});
 
 httpsServer.listen(SSLPORT, function () {
 	console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
