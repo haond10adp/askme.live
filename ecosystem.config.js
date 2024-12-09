57d494045ebd94eae7159e1e63da67759aeefac7
Hao <haond10adp@gmail.com>
2024-12-09 14:11:54 +0700

    41

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'sk-server',
-      script: './sk-server/build/ --node-args="--env-file=.env"',
+      script: './sk-server/build/index.js --node-args="--env-file=.env"',
       env: {
         PORT: 443,
         NODE_ENV: 'development',
@@ -13,7 +13,7 @@ module.exports = {
     },
     {
       name: 'socket-server',
-      script: './socket-server/dist/',
+      script: './socket-server/dist/index.js',
     },
   ],
 };
