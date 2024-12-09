3b28b86b72495425d978ab8db6094962ee4b7dfa
Hao <haond10adp@gmail.com>
2024-12-09 19:22:25 +0700

    43

--- sk-server/src/lib/socket.ts
+++ sk-server/src/lib/socket.ts
@@ -1,4 +1,4 @@
-// export const backendLink = 'https://askmelive.site:3001';
-export const backendLink = 'https://localhost:3001';
+export const backendLink = 'https://askmelive.site:3001';
+// export const backendLink = 'https://localhost:3001';
 import { io } from 'socket.io-client';
 export const socket = io(backendLink);
